b20865355fa689fc1f8e92b14febd46abc9c1fc7
Randy Lai <randy.cs.lai@gmail.com>
2018-04-19 11:33:08 -0400

    add docs

---
+++ R/dict.R
@@ -0,0 +1,39 @@
+#' @export
+Dict <- R6::R6Class("Dict",
+    private = list(
+        e = new.env(hash = TRUE)
+    ),
+    public = list(
+        set = function(key, value) {
+            assign(key, value, envir = private$e)
+        },
+        get = function(key, default = NULL) {
+            private$e[[key]]
+        },
+        remove = function(key) {
+            rm(list = key, envir = private$e)
+        },
+        pop = function(key, default = NULL) {
+            v <- self$get(key, default)
+            self$remove(key)
+            v
+        },
+        has = function(key) {
+            key %in% ls(private$e)
+        },
+        keys = function() {
+            ls(private$e)
+        },
+        values = function() {
+            self$as_list()
+        },
+        update = function(d) {
+            for (key in d$keys()) {
+                self$set(key, d$get(key))
+            }
+            self
+        },
+        size = function() length(ls(private$e)),
+        as_list = function() as.list(private$e)
+    )
+)
